4afa915f1c8447306aa0beca4eaa7486439d9943
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2019-12-28 15:20:44 +0530

    Home Page.

--- config/dbConf.js
+++ config/dbConf.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
     user: process.env.DEV_DB_USER,
     password: process.env.DEV_DB_PASSWORD,
-    connectString: ""
+    connectString: process.env.DEV_DB_CONNECTIONSTRING
 };
--- routers/index.js
+++ routers/index.js
@@ -5,9 +5,11 @@ const envName = process.env.NODE_ENV;
 const oracledb = require('oracledb');
 const dbConfig = require('../config/dbConf');
 console.log(envName);
-var resultSet ="";
+var Results ="";
+var numRows = 0;
 let connection;
-binds = {};
+var resultSetData = {};
+var envData = [];
 
 // For a complete list of options see the documentation.
 options = {
@@ -16,15 +18,61 @@ options = {
     // fetchArraySize: 100       // internal buffer allocation size for tuning
 };
 
-run();
 
-async function run() {
-    if(envName==='dev'){
+if(envName==='dev'){
+    run();
+    async function run() {
         try{
             connection = await oracledb.getConnection(dbConfig);
-            resultSet = await connection.execute(`select COUNT(*) mon_appl_cntr, environment_name FROM dev_env_appl GROUP BY environment_name`);
-            console.log(resultSet.rows);
-            envData=resultSet.rows;
+            Results = await connection.execute(`SELECT COUNT(*) MON_APPL_CNTR, ENVIRONMENT_NAME FROM DEV_ENV_APPL GROUP BY ENVIRONMENT_NAME UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'DEV1_UNAV_APP' FROM DEV1_CONFIG_DATA WHERE APPL_SERVICE_STATUS='CRITICAL' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'DEV1_AV_PROC' FROM DEV1_CONFIG_DATA WHERE APPL_SERVICE_TYPE='PROCESSES' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'DEV1_AV_WEB' FROM DEV1_CONFIG_DATA WHERE APPL_SERVICE_TYPE='WEBSERVICES' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'DEV1_AV_GUI' FROM DEV1_CONFIG_DATA WHERE APPL_SERVICE_TYPE='GUISERVICES' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'DEV2_UNAV_APP' FROM DEV2_CONFIG_DATA WHERE APPL_SERVICE_STATUS='CRITICAL' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'DEV2_AV_PROC' FROM DEV2_CONFIG_DATA WHERE APPL_SERVICE_TYPE='PROCESSES' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'DEV2_AV_WEB' FROM DEV2_CONFIG_DATA WHERE APPL_SERVICE_TYPE='WEBSERVICES' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'DEV2_AV_GUI' FROM DEV2_CONFIG_DATA WHERE APPL_SERVICE_TYPE='GUISERVICES' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'DEV1_UNAV_DB' FROM DEV1_DB_CONFIG_DATA WHERE APPL_SERVICE_STATUS='CRITICAL' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'DEV1_SERVTYP_DB' FROM DEV1_DB_CONFIG_DATA WHERE APPL_SERVICE_TYPE='DB' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'DEV1_SERVTYP_GG' FROM DEV1_DB_CONFIG_DATA WHERE APPL_SERVICE_TYPE='GG' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'DEV2_UNAV_DB' FROM DEV2_DB_CONFIG_DATA WHERE APPL_SERVICE_STATUS='CRITICAL' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'DEV2_SERVTYP_DB' FROM DEV2_DB_CONFIG_DATA WHERE APPL_SERVICE_TYPE='DB' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'DEV2_SERVTYP_GG' FROM DEV2_DB_CONFIG_DATA WHERE APPL_SERVICE_TYPE='GG'`,
+            [], // no bind variables
+            {
+              resultSet: true // return a ResultSet (default is false)
+            }
+          );
+          const rs = Results.resultSet;
+          let row;
+
+          while ((row = await rs.getRow())) {
+            //console.log(row);
+            resultSetData = {
+                "mon_appl_cntr" : row[0],    //counter variable
+                "environment_name" : row[1]   //environment_name variable
+            };
+            envData.push(resultSetData);
+          }      
+          // always close the ResultSet
+          await rs.close();
+        }
+        catch(err){
+            console.error(err);
+        }
+    }
+}
+else if(envName==='test'){
+    run();
+    async function run() {
+        try{
+            connection = await oracledb.getConnection(dbConfig);
+            Results = await connection.execute(`SELECT COUNT(*) MON_APPL_CNTR, ENVIRONMENT_NAME FROM TEST_ENV_APPL GROUP BY ENVIRONMENT_NAME UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'TEST1_UNAV_APP' FROM TEST1_CONFIG_DATA WHERE APPL_SERVICE_STATUS='CRITICAL' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'TEST1_AV_PROC' FROM TEST1_CONFIG_DATA WHERE APPL_SERVICE_TYPE='PROCESSES' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'TEST1_AV_WEB' FROM TEST1_CONFIG_DATA WHERE APPL_SERVICE_TYPE='WEBSERVICES' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'TEST1_AV_GUI' FROM TEST1_CONFIG_DATA WHERE APPL_SERVICE_TYPE='GUISERVICES' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'TEST2_UNAV_APP' FROM TEST2_CONFIG_DATA WHERE APPL_SERVICE_STATUS='CRITICAL' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'TEST2_AV_PROC' FROM TEST2_CONFIG_DATA WHERE APPL_SERVICE_TYPE='PROCESSES' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'TEST2_AV_WEB' FROM TEST2_CONFIG_DATA WHERE APPL_SERVICE_TYPE='WEBSERVICES' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'TEST2_AV_GUI' FROM TEST2_CONFIG_DATA WHERE APPL_SERVICE_TYPE='GUISERVICES' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'TEST1_UNAV_DB' FROM TEST1_DB_CONFIG_DATA WHERE APPL_SERVICE_STATUS='CRITICAL' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'TEST1_SERVTYP_DB' FROM TEST1_DB_CONFIG_DATA WHERE APPL_SERVICE_TYPE='DB' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'TEST1_SERVTYP_GG' FROM TEST1_DB_CONFIG_DATA WHERE APPL_SERVICE_TYPE='GG' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'TEST2_UNAV_DB' FROM TEST2_DB_CONFIG_DATA WHERE APPL_SERVICE_STATUS='CRITICAL' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'TEST2_SERVTYP_DB' FROM TEST2_DB_CONFIG_DATA WHERE APPL_SERVICE_TYPE='DB' UNION ALL SELECT COUNT(*) MON_APPL_CNTR, 'TEST2_SERVTYP_GG' FROM TEST2_DB_CONFIG_DATA WHERE APPL_SERVICE_TYPE='GG'`,
+            [], // no bind variables
+            {
+              resultSet: true // return a ResultSet (default is false)
+            }
+          );
+          const rs = Results.resultSet;
+          let row;
+
+          while ((row = await rs.getRow())) {
+            //console.log(row);
+            resultSetData = {
+                "mon_appl_cntr" : row[0],    //counter variable
+                "environment_name" : row[1]   //environment_name variable
+            };
+            envData.push(resultSetData);
+          }      
+          // always close the ResultSet
+          await rs.close();
         }
         catch(err){
             console.error(err);
@@ -33,16 +81,16 @@ async function run() {
 }
 
 router.get('/', function(req, res) {
-    res.render('main/index', {title: 'Environment Management Dashboard', navbar_title: 'Welcome to Environment Configuration Managed System',
-    monServdataList: envData}, function(err, html) {
+    res.render('main/index', {title: 'Environment Management Dashboard', navbar_title: 'Welcome to Environment Configuration Managed System', 
+        envDataList: envData}, function(err, html) {
         if (err) {
             //res.redirect('/404');
             res.status(404).end('error');
         } else {
-            //console.log(envData);
+            console.log('Data sent to Pug: ',envData);
             res.status(200).send(html);
         }
     });
-  });
+});
 
 module.exports = router;
