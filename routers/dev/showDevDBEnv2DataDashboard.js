c537071244c9db170cca4db5929321c84a1f350d
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2020-01-20 01:21:59 +0530

    adding dashboard.

--- routers/dev/showDevDBEnv2DataDashboard.js
+++ routers/dev/showDevDBEnv2DataDashboard.js
@@ -15,8 +15,6 @@ options = {
     // fetchArraySize: 100       // internal buffer allocation size for tuning
 };
 
-
-
 function loadDashboardDataForDevDBEnv2Page(){
     
     db2appData = [];
@@ -25,10 +23,22 @@ function loadDashboardDataForDevDBEnv2Page(){
     async function run() {
         try{
             connection = await oracledb.getConnection(dbConfig);
-            Results = await connection.execute(`SELECT serv.application_name, srv_stat.APPL_SERVICE_STATUS, coalesce(TBL.CNTR , 0) COUNTS FROM (SELECT distinct application_name FROM dev2_db_config_data) SERV CROSS JOIN (SELECT 'CRITICAL' AS APPL_SERVICE_STATUS FROM DUAL UNION SELECT 'HIGH' AS APPL_SERVICE_STATUS FROM DUAL UNION SELECT 'NORMAL' AS APPL_SERVICE_STATUS FROM DUAL) SRV_STAT LEFT JOIN (SELECT APPLICATION_NAME, APPL_SERVICE_STATUS, COUNT(*) CNTR FROM dev2_db_config_data GROUP BY APPLICATION_NAME, APPL_SERVICE_STATUS) TBL ON TBL.APPLICATION_NAME = SERV.APPLICATION_NAME AND SRV_STAT.APPL_SERVICE_STATUS = TBL.APPL_SERVICE_STATUS ORDER BY serv.application_name, srv_stat.appl_service_status`,
+            Results = await connection.execute(`select APPLICATION_NAME,  'TOTAL NO OF MONITORED SERVICES' as "APPL_SERVICE_STATUS" , COUNTS FROM (
+                select APPLICATION_NAME,  COUNT(*) COUNTS FROM dev2_db_config_data GROUP BY APPLICATION_NAME ORDER BY APPLICATION_NAME)
+                UNION ALL
+                select APPLICATION_NAME, APPL_SERVICE_STATUS, COUNTS  from 
+                (SELECT serv.application_name, srv_stat.APPL_SERVICE_STATUS, coalesce(TBL.CNTR , 0) COUNTS 
+                FROM (SELECT distinct application_name FROM dev2_db_config_data) SERV 
+                CROSS JOIN (SELECT 'CRITICAL' AS APPL_SERVICE_STATUS FROM DUAL 
+                        UNION SELECT 'HIGH' AS APPL_SERVICE_STATUS FROM DUAL 
+                        UNION SELECT 'NORMAL' AS APPL_SERVICE_STATUS FROM DUAL) SRV_STAT 
+                LEFT JOIN (SELECT APPLICATION_NAME, APPL_SERVICE_STATUS, COUNT(*) CNTR 
+                FROM dev2_db_config_data GROUP BY APPLICATION_NAME, APPL_SERVICE_STATUS) TBL 
+                ON TBL.APPLICATION_NAME = SERV.APPLICATION_NAME AND SRV_STAT.APPL_SERVICE_STATUS = TBL.APPL_SERVICE_STATUS 
+                ORDER BY serv.application_name, srv_stat.appl_service_status desc)`,
             [], // no bind variables
             {
-            resultSet: true // return a ResultSet (default is false)
+                resultSet: true // return a ResultSet (default is false)
             });
             const rs = Results.resultSet;
             let row;
@@ -36,8 +46,8 @@ function loadDashboardDataForDevDBEnv2Page(){
             while ((row = await rs.getRow())) {
                 resultSetData = {
                     "application_name" : row[0],            //application_name variable
-                    "appl_service_status" : row[1],          //appl_package_name variable
-                    "counter" : row[2],          //appl_package_name variable
+                    "appl_service_status" : row[1],         //appl_service_status variable
+                    "counter" : row[2],                     //counter variable
                 };
                 db2appData.push(resultSetData);
             }      
@@ -54,11 +64,11 @@ function loadDashboardDataForDevDBEnv2Page(){
                 console.error(err);
               }
             }
-          }
+        }
     }
 
     router.get('/', function(req, res) {
-        res.render('dev/showDev2DBDataDasboard', {title: 'Environment Management Dashboard', navbar_title: 'Welcome to Environment Configuration Managed System',
+        res.render('dev/showDev2DBDataDashboard', {title: 'Environment Management Dashboard', navbar_title: 'Welcome to Environment Configuration Managed System',
         db2DataList: db2appData}, function(err, html) {
             if (err) {
                 //res.redirect('/404');
